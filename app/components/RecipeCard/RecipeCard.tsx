f1ddfafedef94fd9d78b6476a20b29312fb44d31
M-vPham <michellepham000@gmail.com>
2024-02-25 23:32:09 -0500

    Added recipeImageSrc into the interface and created a default value for the card

--- app/components/RecipeCard/RecipeCard.tsx
+++ app/components/RecipeCard/RecipeCard.tsx
@@ -3,19 +3,24 @@ import Image from "next/image";
 
 interface Recipe {
   recipeTitle: string;
+  recipeImageSrc?: string;
   recipeDescription: String;
   recipeCatagory: String;
 }
 
 const RecipeCard = ({
   recipeTitle,
+  recipeImageSrc,
   recipeDescription,
   recipeCatagory,
 }: Recipe) => {
   return (
     <div className="card card-compact w-60 bg-base-100 shadow-xl">
       <figure>
-        <img src="/static/images/bun-bo-hue.jpg" alt="Shoes" />
+        <img
+          src={(recipeImageSrc = "/static/images/bun-bo-hue.jpg")}
+          alt={recipeTitle}
+        />
       </figure>
       <div className="card-body bg-slate-800">
         <h2 className="card-title">{recipeTitle}</h2>
