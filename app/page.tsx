5177f7f1588379d7b02bf4912f3df60c2bb1f655
M-vPham <michellepham000@gmail.com>
2024-02-27 23:45:08 -0500

    Connected to a mongodb server.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Link from "next/link";
-import FilterBar from "../components/FilterBar/FilterBar";
 
 export default function Home() {
   return (
     <main>
       <p> Hello World</p>
-      <FilterBar />
       <Link href="/allRecipes">Recipes</Link>
     </main>
   );
