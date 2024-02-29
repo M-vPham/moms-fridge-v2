fb31dca4cc3a8c800344e157eb4cfce655a06623
M-vPham <michellepham000@gmail.com>
2024-02-29 14:46:20 -0500

    Switched to startStandaloneServer in apollo/server package

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,44 +1,28 @@
-import { startServerAndCreateNextHandler } from "@as-integrations/next";
+// @ts-nocheck
 import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { NextRequest } from "next/server";
-import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from '@apollo/server/plugin/landingPage/default';
-import mongoose from "mongoose";
+import { Mongoose } from "mongoose";
+import {gql} from "graphql-tag";
 
-import typeDefs from "./schema/typeDefs";
-import resolvers from "./schema/resolvers";
+import { MONGODB_URI } from "@/app/lib/constants";
+import { MyContext } from "@/app/interfaces/type";
 
-//you can view the apollo playground on localhost:3000/api/graphql
-
-const uri = "mongodb+srv://michellepham000:test@cluster-0.niqmbaz.mongodb.net/";
-
-const connectDB = async () => {
-  try {
-    if (uri) {
-      await mongoose.connect(uri);
-      console.log("🎉 connected to database successfully");
-    }
-  } catch (error) {
-    console.error(error);
+const typeDefs = gql`
+  type Query {
+    hello: String
   }
-};
-connectDB();
+`;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers: resolvers,
-  plugins: [
-    // Install a landing page plugin based on NODE_ENV
-    process.env.NODE_ENV === 'production'
-      ? ApolloServerPluginLandingPageProductionDefault({
-          graphRef: 'my-graph-id@my-graph-variant',
-          footer: false,
-        })
-      : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
-  ],
+const resolvers = gql`
+  Query: {
+    hello: () => "Hello World"
+  }
+`;
+
+const server = new ApolloServer<MyContext>({ typeDefs, resolvers });
+const { url } = await startStandaloneServer(server, {
+  context: async ({ req }) => ({ token: req.headers.token }),
+  listen: { port: 4000 },
 });
-
-const handler = startServerAndCreateNextHandler<NextRequest>(server, {
-  context: async req => ({ req }),
-});
-
-export { handler as GET, handler as POST };
+console.log(`🚀  Server ready at ${url}`);
