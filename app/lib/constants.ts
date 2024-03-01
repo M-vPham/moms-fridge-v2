d1da4d152b57eaf2949958070f806ea3237d0813
M-vPham <michellepham000@gmail.com>
2024-03-01 16:06:07 -0500

    Added an .env file for sensitive information

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,1 +1,4 @@
-export const MONGODB_URI = "mongodb+srv://michellepham000:test@cluster-0.niqmbaz.mongodb.net/";
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_URI_USERNAME}:${process.env.MONGODB_URI_PASSWORD}@cluster-0.niqmbaz.mongodb.net/`;
