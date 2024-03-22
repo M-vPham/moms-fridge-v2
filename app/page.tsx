27de94a597d3b0f9cf6d2a0862e6e86e0d58d3c3
M-vPham <michellepham000@gmail.com>
2024-03-22 17:08:43 -0400

    Created aboutUs route

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <p> Hello World</p>
+      <Link href="/aboutUs">About Us</Link>
       <Link href="/allRecipes">Recipes</Link>
     </div>
   );
