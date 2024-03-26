796102eb7b6346663e019a06879518add2459f35
M-vPham <michellepham000@gmail.com>
2024-03-25 23:32:44 -0400

    Added filter bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Link from "next/link";
-
+import FilterBar from "@/components/FilterBar/FilterBar";
 export default function Home() {
   return (
+    <FilterBar/>
     <div>
       <Link href="/aboutUs">About Us</Link>
       <Link href="/allRecipes">Recipes</Link>
