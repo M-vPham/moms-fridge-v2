f3594e519d67237a0697bb4c251e9d11ed480c31
M-vPham <michellepham000@gmail.com>
2024-03-26 15:25:07 -0400

    Prototype for the navbar on the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import Link from "next/link";
 import FilterBar from "@/components/FilterBar/FilterBar";
 export default function Home() {
-  return (
-    <FilterBar/>
-    <div>
-      <Link href="/aboutUs">About Us</Link>
-      <Link href="/allRecipes">Recipes</Link>
-    </div>
-  );
+  return <FilterBar />;
 }
--- components/FilterBar/FilterBar.tsx
+++ components/FilterBar/FilterBar.tsx
@@ -1,14 +1,38 @@
-"use client";
 import React from "react";
-import styles from "./FilterBar.module.css";
+import Link from "next/link";
 const FilterBar = () => {
   return (
-    <div className={styles.filterBar}>
-      <input
-        type="text"
-        placeholder="Type here"
-        className="input input-bordered w-full max-w-xs"
-      />
+    <div className="navbar">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Mom's Fridge</a>
+      </div>
+      <ul className="menu menu-horizontal px-4">
+        <Link className="menu menu-horizontal px-1" href="/allRecipes">
+          Recipes
+        </Link>
+        <Link className="menu menu-horizontal px-1" href="/aboutUs">
+          About Us
+        </Link>
+        <div className="flex items-center">
+          <input className="bg-slate-50 hover:bg-red-200 rounded-3xl h-12 w-56" />
+          <button className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </ul>
     </div>
   );
 };
